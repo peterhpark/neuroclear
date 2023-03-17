19d554b1cb319afbc684aabe6051d907654e245c
peterhpark <peterpark828@icloud.com>
2023-03-17 15:50:05 +0100

    Thor model implemented

--- models/axial_to_lateral_gan_thor_model.py
+++ models/axial_to_lateral_gan_thor_model.py
@@ -5,13 +5,13 @@ from .base_model import BaseModel
 from torchmetrics import StructuralSimilarityIndexMeasure
 from . import networks
 
-class AxialToLateralGANIdunModel(BaseModel):
+class AxialToLateralGANThorModel(BaseModel):
     """
     This model uses high-resolution reference from another source.
     The model takes a 3D image cube as an input and outputs a 3D image stack that correspond to the output cube.
     Note that the loss functions are readjusted for cube dataset.
 
-    This model is a successor to Bragi; it replaces the discriminators in the backward path with L2 loss.
+    This model is a successor to Bragi; it pre-trains the G for supervised learning later by using a frozen F.
 
     GAN Loss is calculated in 2D between axial image and lateral image. -> Discriminator takes 2D images
                                                                         -> Generator takes 3D images.
@@ -22,6 +22,9 @@ class AxialToLateralGANIdunModel(BaseModel):
     D_A_axial: ref_XY <-> isotropic_axial_MIP
     D_A_lateral: ref_XY <-> isotropic_lateral_MIP
 
+    D_B_axial: original_axial <-> reconstructed_axial
+    D_B_lateral: original_lateral <-> reconstructed_lateral
+
     We only consider one path: A->B.
     We also do not consider buffering fake images for discriminator.
 
@@ -30,7 +33,6 @@ class AxialToLateralGANIdunModel(BaseModel):
     @staticmethod
     def modify_commandline_options(parser, is_train=True):
         parser.set_defaults(no_dropout=True)  # default CycleGAN did not use dropout
-
         if is_train:
             parser.add_argument('--lambda_A', type=float, default=10.0, help='weight for cycle loss (A -> B -> A)')
             parser.add_argument('--gan_mode', type=str, default='vanilla',
@@ -60,21 +62,15 @@ class AxialToLateralGANIdunModel(BaseModel):
         """
 
         BaseModel.__init__(self, opt)
-        # specify the training losses you want to print out. The training/test scripts will call <BaseModel.get_current_losses>
 
         if opt.data_gt is not None:
             self.validate = True
         else:
             self.validate = False
 
-        self.loss_names = ['D_A_lateral', 'D_A_axial', 'G_A', 'G_A_lateral', 'G_A_axial', 'cycle']
-
-        # specify the images you want to save/display. The training/test scripts will call <BaseModel.get_current_visuals>
-        self.visual_names = ['real_tgt', 'real_src', 'fake', 'rec']
-
-        if self.validate:
-            self.loss_names += ['valL1', 'valssim']
-            self.visual_names += ['real_gt']
+        # specify the training losses you want to print out. The training/test scripts will call <BaseModel.get_current_losses>
+        self.loss_names = ['D_A_lateral', 'D_A_axial', 'G_A', 'G_A_lateral', 'G_A_axial', 'cycle',
+                           'D_B_lateral', 'D_B_axial', 'G_B', 'G_B_lateral', 'G_B_axial']
 
         self.gan_mode = opt.gan_mode
 
@@ -91,10 +87,15 @@ class AxialToLateralGANIdunModel(BaseModel):
 
         self.sample_proj = opt.projection_sampling
 
+        # specify the images you want to save/display. The training/test scripts will call <BaseModel.get_current_visuals>
+        self.visual_names = ['real_tgt', 'real_src', 'fake', 'rec']
+
+        if self.validate:
+            self.loss_names += ['valL1', 'valssim']
+            self.visual_names += ['real_gt']
 
         self.lambda_plane_target, self.lambda_slice, self.lambda_proj = [
             factor / (opt.lambda_plane[0] + opt.lambda_plane[1] + opt.lambda_plane[2]) for factor in opt.lambda_plane]
-
 
         self.lateral_axis = 0  # XY plane
         self.axial_1_axis = 1 # XZ plane
@@ -102,13 +103,13 @@ class AxialToLateralGANIdunModel(BaseModel):
 
         # specify the models you want to save to the disk. The training/test scripts will call <BaseModel.save_networks> and <BaseModel.load_networks>.
         if self.isTrain:
-            self.model_names = ['G_A', 'G_B', 'D_A_lateral', 'D_A_axial']
+            self.model_names = ['G_A', 'G_B', 'D_A_lateral', 'D_A_axial', 'D_B_lateral', 'D_B_axial']
         else:  # during test time, only load Gs
             self.model_names = ['G_A', 'G_B']
 
         # define networks (both Generators and discriminators)
         # The naming is different from those used in the paper.
-        # Code (vs. paper): G_A (G), G_B (F), D_A (D_Y)
+        # Code (vs. paper): G_A (G), G_B (F), D_A (D_Y), D_B (D_X)
         self.netG_A = networks.define_G(opt.input_nc, opt.output_nc, opt.ngf, opt.netG, opt.norm,
                                         not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids,
                                         dimension=self.gen_dimension)
@@ -126,6 +127,15 @@ class AxialToLateralGANIdunModel(BaseModel):
                                                     opt.n_layers_D, opt.norm, opt.init_type, opt.init_gain, False,
                                                     self.gpu_ids, dimension=self.dis_dimension)
 
+
+            self.netD_B_axial = networks.define_D(opt.input_nc, opt.ndf, opt.netD,
+                                                  opt.n_layers_D, opt.norm, opt.init_type, opt.init_gain, False,
+                                                  self.gpu_ids, dimension=self.dis_dimension)
+
+            self.netD_B_lateral = networks.define_D(opt.input_nc, opt.ndf, opt.netD,
+                                                    opt.n_layers_D, opt.norm, opt.init_type, opt.init_gain, False,
+                                                    self.gpu_ids, dimension=self.dis_dimension)
+
         if self.isTrain:
             # define loss functions
             self.criterionGAN = networks.GANLoss(opt.gan_mode).to(self.device)  # define GAN loss.
@@ -139,7 +149,8 @@ class AxialToLateralGANIdunModel(BaseModel):
             self.optimizer_G = torch.optim.Adam(itertools.chain(self.netG_A.parameters(), self.netG_B.parameters()),
                                                 lr=opt.lr, betas=(opt.beta1, 0.999))
             self.optimizer_D = torch.optim.Adam(
-                itertools.chain(self.netD_A_axial.parameters(), self.netD_A_lateral.parameters()),
+                itertools.chain(self.netD_A_axial.parameters(), self.netD_A_lateral.parameters(),
+                                self.netD_B_axial.parameters(), self.netD_B_lateral.parameters()),
                 lr=opt.lr, betas=(opt.beta1, 0.999))
             self.optimizers.append(self.optimizer_G)
             self.optimizers.append(self.optimizer_D)
@@ -179,6 +190,35 @@ class AxialToLateralGANIdunModel(BaseModel):
         """
         self.fake = self.netG_A(self.real_src)  # G_A(A)
         self.rec = self.netG_B(self.fake)  # G_B(G_A(A))
+
+    # def backward_D_slice(self, netD, real, fake, slice_axis_real, slice_axis_fake):
+    #
+    #     """Calculate GAN loss for the discriminator
+    #
+    #     Parameters:
+    #         netD (network)      -- the discriminator D
+    #         real (tensor array) -- real images
+    #         fake (tensor array) -- images generated by a generator
+    #
+    #     Return the discriminator loss.
+    #     We also call loss_D.backward() to calculate the gradients.
+    #     """
+    #
+    #     # Real
+    #     pred_real = self.iter_f(real, netD, slice_axis_real)
+    #     pred_fake = self.iter_f(fake.detach(), netD, slice_axis_fake)
+    #
+    #     # real
+    #     loss_D_real = self.criterionGAN(pred_real, True)  # Target_is_real -> True: loss (pred_real - unit vector)
+    #
+    #     # Fake
+    #     loss_D_fake = self.criterionGAN(pred_fake, False)  # no loss with the unit vector
+    #
+    #     # Combined loss and calculate gradients
+    #     loss_D = (loss_D_real + loss_D_fake) * 0.5
+    #
+    #     loss_D.backward()
+    #     return loss_D
 
     def backward_D_projection(self, netD, real, fake, slice_axis_real, slice_axis_fake):
 
@@ -222,6 +262,20 @@ class AxialToLateralGANIdunModel(BaseModel):
 
         self.loss_D_A_axial = (self.loss_D_A_axial_1 + self.loss_D_A_axial_2)*0.5
 
+    def backward_D_B_lateral(self):
+        self.loss_D_B_lateral = self.backward_D_projection(self.netD_B_lateral, self.real_src, self.rec, self.lateral_axis,
+                                                      self.lateral_axis)  # comparing XY_original to XY_reconstructed
+
+    def backward_D_B_axial(self): # compares real_tgt axial slice image and fake axial slice image.
+        """Calculate GAN loss for discriminator D_B, which compares the original and the reconstructed. """
+        self.loss_D_B_axial_1 = self.backward_D_projection(self.netD_B_axial, self.real_src, self.rec, self.axial_1_axis,
+                                                      self.axial_1_axis)  # comparing YZ_original to YZ_reconstructed
+
+        self.loss_D_B_axial_2 = self.backward_D_projection(self.netD_B_axial, self.real_src, self.rec, self.axial_2_axis,
+                                                      self.axial_2_axis)  # comparing YZ_original to YZ_reconstructed
+
+        self.loss_D_B_axial = (self.loss_D_B_axial_1 + self.loss_D_B_axial_2)*0.5
+
     def backward_G(self):
         """Calculate the loss for generators G_A and G_B"""
         lambda_A = self.opt.lambda_A
@@ -236,6 +290,15 @@ class AxialToLateralGANIdunModel(BaseModel):
 
         self.loss_G_A = self.loss_G_A_lateral + self.loss_G_A_axial * 0.5
 
+        self.loss_G_B_lateral = self.criterionGAN(self.proj_f(self.rec, self.netD_B_lateral, self.lateral_axis),
+                                                  True) * self.lambda_plane_target
+        self.loss_G_B_axial = self.criterionGAN(self.proj_f(self.rec, self.netD_B_axial, self.axial_1_axis),
+                                                True) * self.lambda_slice + \
+                              self.criterionGAN(self.proj_f(self.rec, self.netD_B_axial, self.axial_2_axis),
+                                                True) * self.lambda_slice
+
+        self.loss_G_B = self.loss_G_B_lateral + self.loss_G_B_axial * 0.5
+
         # This model only includes forward cycle loss || G_B(G_A(A)) - A||
         self.loss_cycle = self.criterionCycle(self.rec, self.real_src) * lambda_A
 
@@ -245,7 +308,7 @@ class AxialToLateralGANIdunModel(BaseModel):
             self.loss_valssim = self.criterionValssim(self.fake.detach(), self.real_gt)
 
         # combined loss and calculate gradients
-        self.loss_G = self.loss_G_A + self.loss_cycle
+        self.loss_G = self.loss_G_A + self.loss_G_B + self.loss_cycle
         self.loss_G.backward()
 
     def optimize_parameters(self):
@@ -255,20 +318,22 @@ class AxialToLateralGANIdunModel(BaseModel):
 
         # G_A and G_B
         self.set_requires_grad(
-            [self.netD_A_lateral, self.netD_A_axial], False)  # Ds require no gradients when optimizing Gs
+            [self.netD_A_lateral, self.netD_A_axial, self.netD_B_lateral, self.netD_B_axial], False)  # Ds require no gradients when optimizing Gs
         self.optimizer_G.zero_grad()  # set G_A and G_B's gradients to zero
         self.backward_G()  # calculate gradients for G_A and G_B
         self.optimizer_G.step()  # update G_A and G_B's weights
 
-        # D_A
+        # D_A and D_B
         self.set_requires_grad(
-            [self.netD_A_lateral, self.netD_A_axial], True)
-        self.optimizer_D.zero_grad()  # set D_A's gradients to zero
+            [self.netD_A_lateral, self.netD_A_axial, self.netD_B_lateral, self.netD_B_axial], True)
+        self.optimizer_D.zero_grad()  # set D_A and D_B's gradients to zero
 
         self.backward_D_A_lateral()
         self.backward_D_A_axial()  # calculate gradients for D_A's
 
-        self.optimizer_D.step()  # update D_A's weights
+        self.backward_D_B_lateral()
+        self.backward_D_B_axial()  # calculate gradients for D_B's
+        self.optimizer_D.step()  # update D_A and D_B's weights
 
     def proj_f(self, input, function, slice_axis):
         input_volume = Volume(input, self.device)
