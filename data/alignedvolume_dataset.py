0d41ac2d4f5c85060ffc273b93f76d4eeee8ed5a
peterhpark <peterpark828@icloud.com>
2023-03-21 11:41:20 +0100

    started working on making supervised model for comparison

---
+++ data/alignedvolume_dataset.py
@@ -0,0 +1,74 @@
+from data.base_dataset import BaseDataset, get_transform, get_params
+from options.train_options import TrainOptions
+from data.image_folder import make_dataset
+from skimage import io
+import re
+
+
+def numericalSort(value):
+    numbers = re.compile(r'(\d+)')
+    parts = numbers.split(value)
+    parts[1::2] = map(int, parts[1::2])
+    return parts
+
+
+class AlignedVolumeDataset(BaseDataset):
+    """
+    Loads image volume dataset. The dataset is consisted of multiple 3D image sub-volumes.
+    This dataset loads one volume each from the source and target datasets.
+    """
+
+    @staticmethod
+    def modify_commandline_options(parser, is_train=False):
+        parser.add_argument('--data_ref', help = 'path to reference images')
+        parser.add_argument('--validate', action= 'store_true', help = 'select whether you want to get validation loss while training')
+
+        return parser
+
+    def __init__(self, opt):
+        """Initialize this dataset class.
+
+        Parameters:
+            opt (Option class) -- stores all the experiment flags; needs to be a subclass of BaseOptions
+        """
+
+        BaseDataset.__init__(self, opt)
+        self.A_path = make_dataset(opt.dataroot, 1)[0]  # loads only one image volume.
+        self.A_img_np = io.imread(self.A_path)
+        self.A_img_shape = self.A_img_np.shape
+
+        self.B_path = make_dataset(opt.data_ref, 1)[0]  # loads only one image volume.
+        self.B_img_np = io.imread(self.B_path)
+
+        self.validate = opt.validate
+
+        btoA = self.opt.direction == 'BtoA'
+
+        self.isTrain = opt.isTrain
+
+    def __getitem__(self, index):
+        # apply image transformation
+
+
+        transform_params = get_params(self.opt, self.A_img_shape)
+        transform_A = get_transform(self.opt, params = transform_params)
+        transform_B = get_transform(self.opt, params = transform_params) # still randomize
+
+        A = transform_A(self.A_img_np)
+        B = transform_B(self.B_img_np)
+
+
+        if self.validate:
+            C = transform_A(self.C_img_np)
+            return {'src': A, 'src_paths': self.A_path, 'tgt': B, 'tgt_paths': self.B_path, 'gt': C, 'gt_paths': self.C_path}
+
+        else:
+            return {'src': A, 'src_paths': self.A_path, 'tgt': B, 'tgt_paths': self.B_path}
+
+    def __len__(self):
+        """Return the total number of images in the dataset.
+        As we have two datasets with potentially different number of images,
+        """
+
+        # each epoch is 10 images.
+        return int(10)
--- models/supervised_model.py
+++ models/supervised_model.py
@@ -1,5 +1,6 @@
 import torch
 from .base_model import BaseModel
+from torchmetrics import StructuralSimilarityIndexMeasure
 from . import networks
 
 
@@ -13,8 +14,15 @@ class SupervisedModel(BaseModel):
         Parameters:
             opt (Option class)-- stores all the experiment flags; needs to be a subclass of BaseOptions
         """
+
         assert(not opt.isTrain)
         BaseModel.__init__(self, opt)
+
+        if opt.data_gt is not None:
+            self.validate = True
+        else:
+            self.validate = False
+
         # specify the training losses you want to print out. The training/test scripts  will call <BaseModel.get_current_losses>
         self.loss_names = ['G_L1']
         # specify the images you want to save/display. The training/test scripts  will call <BaseModel.get_current_visuals>
@@ -22,17 +30,21 @@ class SupervisedModel(BaseModel):
         # specify the models you want to save to the disk. The training/test scripts will call <BaseModel.save_networks> and <BaseModel.load_networks>
         self.model_names = ['G' + opt.model_suffix]  # only generator is needed.
         self.dimension = opt.image_dimension
-        print (self.dimension)
 
         self.netG = networks.define_G(opt.input_nc, opt.output_nc, opt.ngf, opt.netG,
                                       opt.norm, not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids, dimension = self.dimension)
 
-        # assigns the model to self.netG_[suffix] so that it can be loaded
-        # please see <BaseModel.load_networks>
-        setattr(self, 'netG' + opt.model_suffix, self.netG)  # store netG in self.
+
 
         if self.isTrain:
             self.criterionL1 = torch.nn.L1Loss()
+            self.criterionssim = StructuralSimilarityIndexMeasure(data_range=1.0).to(self.device)
+
+            if self.validate:
+                self.criterionValL1 = torch.nn.L1Loss() # comparison with GT for validation
+                self.criterionValssim =StructuralSimilarityIndexMeasure(data_range=1.0).to(self.device)
+
+        self.optimizer = torch.optim.Adam(self.netG.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
 
     def set_input(self, input):
         """Unpack input data from the dataloader and perform necessary pre-processing steps.
@@ -40,15 +52,28 @@ class SupervisedModel(BaseModel):
             input: a dictionary that contains the data itself and its metadata information.
         We need to use 'single_dataset' dataset mode. It only load images from one domain.
         """
-        self.real = input['A'].to(self.device)
-        self.image_paths = input['A_paths']
+        AtoB = self.opt.direction == 'AtoB'
+        self.real_src = input['src' if AtoB else 'tgt'].to(self.device)
+        self.real_tgt = input['tgt' if AtoB else 'src'].to(self.device)
+
+        if self.validate:
+            self.real_gt = input['gt'].to(self.device)
+            self.image_paths_gt = input['gt_paths']
+
+        self.image_paths_src = input['src_paths' if AtoB else 'tgt_paths']
+        self.image_paths_tgt = input['tgt_paths' if AtoB else 'src_paths']
+
+        self.cube_shape = self.real_src.shape
+        self.num_slice = self.cube_shape[-3]
 
     def forward(self):
         """Run forward pass."""
-        self.fake = self.netG(self.real)  # G(real)
+        self.fake = self.netG(self.real_tgt)  # G(real)
+        self.fake_val = self.netG(self.real_src)
+
 
     def backward_G(self):
-        self.cri
+        self.criterionL1(self.real, )
 
     def optimize_parameters(self):
         """No optimization for test model."""
